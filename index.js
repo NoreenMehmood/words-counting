8f47a56e2bf859564553f0b6f363a61cc98fa12c
Noreen Mehmood <mehmood.noreen@gmail.com>
2024-04-25 02:48:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt({
+    name: "words",
+    type: "input",
+    message: "Enter a sentences",
+});
+let word_count = user_ans.words.trim().split(" ").length;
+console.log(`Your sentence has ${word_count} words.`);
